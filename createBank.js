1510b43c2285520d5ad3fb0b495fb9c4e39e2f6f
Awallence <awallencecode@gmail.com>
2026-04-02 22:19:08 +0300

    implement user creation functionality in createBank.js

--- createBank.js
+++ createBank.js
@@ -1,2 +1,54 @@
 const prompt = require("prompt-sync")();
 
+//function for
+function createUser(name, age, deposit, withdraw)
+{
+    let balance = 0;
+
+    return {
+        name: name,
+        age: age,
+        deposit: deposit,
+        withdraw: withdraw,
+
+        name: function()
+        {
+            return name;
+        },
+
+        age: function()
+        {
+            return age;
+        },
+
+        deposit: function(amount)
+        {
+            return balance += amount;
+        },
+
+        withdraw: function(amount)
+        {
+            return balance -= amount;
+        },
+
+        getBalanceUser: function()
+        {
+            return balance;
+        },
+
+        getInfoUser: function()
+        {
+            console.log(`User name -> ${this.name}\nUser age -> ${this.age}\nUser balance -> ${this.balance}`);
+        }
+    }
+    
+}
+
+
+//main function for for compiling the code 
+function main()
+{
+
+}
+
+main();
