f65b86a39885534b79015029a3a8320e40c50dd3
Awallence <awallencecode@gmail.com>
2026-04-03 16:30:17 +0300

    implement main function in createBank.js to handle user interactions for banking operations including deposit, withdrawal, balance check, and user info display

--- createBank.js
+++ createBank.js
@@ -93,7 +93,52 @@ function enterUserName()
 //main function for compiling the code 
 function main()
 {
-    
+    let userName = enterUserName();
+    let userAge = enterUserAge();
+
+    let user_1 = createUser(userName, userAge);
+
+    while(true)
+    {
+        console.log("Press 1 for deposit your balance\nPress 2 for withdraw your balance\nPress 3 for check your balance\PPress 4 for check your info\nPress 5 for exit");
+
+        let userChoice = Number(prompt("Enter your choice -> "));
+
+        switch(userChoice)
+        {
+            case 1:
+                {
+                    break;
+                }
+
+            case 2:
+                {
+                    break;
+                }
+            
+            case 3:
+                {
+                    break;
+                }
+
+            case 4:
+                {
+                    break;
+                }
+            
+            case 5:
+                {
+                    break;
+                }
+
+            default:
+                {
+                    console.log("There is no such answer option");
+                    return;
+                }
+
+        }
+    }
 }
 
 main();
